f4669b1fd063f9d7b1634cd6efed2ee035c6133b
Aayush Agarwal <itsaayush3@gmail.com>
2026-07-22 13:06:19 +0530

    Time: 8 ms (13.96%), Space: 35 MB (9.67%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -5,13 +5,13 @@ public:
         for(auto num: nums)
             hash[num]++;
         int v = *max_element(hash.begin(),hash.end());
-        vector <vector<int>> res(v);
+        vector <vector<int>> ans(v);
         for(int i=0; i < 201; i++){
             int k = hash[i];
             for(int j=0; j < k; j++){
-                res[j].push_back(i);
+                ans[j].push_back(i);
             }
         }
-        return res;
+        return ans;
     }
 };

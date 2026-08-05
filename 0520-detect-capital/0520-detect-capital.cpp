966042608597e701b0df974c049d81083d4f2778
Aayush Agarwal <itsaayush3@gmail.com>
2026-08-05 22:42:21 +0530

    Time: 1 ms (7.96%), Space: 8.2 MB (15.21%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for(char c: word){
+            if(isupper(c))
+                count++;
+        }
+        if(count == 1 && isupper(word[0]) || count == 0)
+            return true;
+        return (word.size() == count); 
+    }
+};

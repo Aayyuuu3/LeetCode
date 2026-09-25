cf8ff03c8d0b14e1e19b30dc917576720113598d
Aayush Agarwal <itsaayush3@gmail.com>
2026-09-26 01:39:28 +0530

    Time: 35 ms (28.52%), Space: 177.8 MB (60.81%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -4,7 +4,6 @@ public:
         if(n == 1) return costs[0] + 1;
 
         vector<int> dp(n + 1);
-        
         dp[0] = 0;
         dp[1] = costs[0] + 1;
         dp[2] = min(dp[1] + costs[1] + 1, costs[1] + 4);
@@ -15,7 +14,6 @@ public:
             int c = costs[i-1] + dp[i-1] + 1;
             dp[i] = min({a,b,c});
         }
-        
         return dp[n];
     }
 };
